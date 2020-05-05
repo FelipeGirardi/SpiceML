f904512c3a4a97820bd3e5ceb3b7f0707175d50b
Felipe Girardi <felipegirardifg@gmail.com>
2020-05-05 18:09:16 -0300

    Finish identifySpice screen layout

--- SpiceML/View Controllers/IdentifySpiceViewController.swift
+++ SpiceML/View Controllers/IdentifySpiceViewController.swift
@@ -15,18 +15,43 @@ class IdentifySpiceViewController: UIViewController {
     var classProbability: Double?
     
     @IBOutlet weak var spiceImage: UIImageView!
-
+    @IBOutlet weak var spiceNameLabel: UILabel!
+    @IBOutlet weak var titleBackgroundView: UIView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var howToConsumeTextView: UITextView!
+    @IBOutlet weak var howToConsumeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        spiceImage.image = selectedImage
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.isTranslucent = true
-        self.navigationController?.view.backgroundColor = .clear
+        spiceImage.image = selectedImage
+        spiceNameLabel.text = classLabel?.uppercased()
+        
+        titleBackgroundView.roundCorners(corners: [.topLeft, .topRight], radius: 20)
+        
+        howToConsumeLabel.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        descriptionTextView.sizeToFit()
+        howToConsumeTextView.sizeToFit()
+        
+        
     }
 
+}
+
+extension UIView {
+
+    func roundCorners(corners:UIRectCorner, radius: CGFloat) {
+
+        DispatchQueue.main.async {
+            let path = UIBezierPath(roundedRect: self.bounds,
+                                    byRoundingCorners: corners,
+                                    cornerRadii: CGSize(width: radius, height: radius))
+            let maskLayer = CAShapeLayer()
+            maskLayer.frame = self.bounds
+            maskLayer.path = path.cgPath
+            self.layer.mask = maskLayer
+        }
+    }
 }
